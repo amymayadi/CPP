24d51458bc2e98a52e688427f57165b148e36993
马雅狄 <mayadi@travelsky.com>
2018-08-10 16:14:56 +0800

    const_cast cause undefined behavior
    const_cast应使用于，预知为非const的变量而不是const变量，通常用于常量指针或常量引用的去常量

--- PointerConstant4Cast/const.cpp
+++ PointerConstant4Cast/const.cpp
@@ -4,6 +4,7 @@ int main()
 {
     const int a = 5;
     //int &b = a;  // 编译错误，不允许指向const变量。
+    // 这是一个undefined behavior，const_cast应使用于，预知为非const的变量而不是const变量，通常用于常量指针或常量引用的去常量
     int &b = const_cast<int&>(a);
     std::cout << "&a=" << &a << std::endl;
     std::cout << "&b=" << &b << std::endl;
